f6584d96ac014a118b9052fbb6161b61d75b52a7
velocitywh <91432942+velocitywh@users.noreply.github.com>
2023-10-03 10:45:31 -0700

    push1

--- bienesraices_fin/src/js/app.js
+++ bienesraices_fin/src/js/app.js
@@ -1,44 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  eventListeners();
 
-    eventListeners();
-
-    darkMode();
+  darkMode();
 });
 
 function darkMode() {
+  const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 
-    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  // console.log(prefiereDarkMode.matches);
 
-    // console.log(prefiereDarkMode.matches);
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
 
-    if(prefiereDarkMode.matches) {
-        document.body.classList.add('dark-mode');
+  prefiereDarkMode.addEventListener("change", function () {
+    if (prefiereDarkMode.matches) {
+      document.body.classList.add("dark-mode");
     } else {
-        document.body.classList.remove('dark-mode');
+      document.body.classList.remove("dark-mode");
     }
+  });
 
-    prefiereDarkMode.addEventListener('change', function() {
-        if(prefiereDarkMode.matches) {
-            document.body.classList.add('dark-mode');
-        } else {
-            document.body.classList.remove('dark-mode');
-        }
-    });
-
-    const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-    });
+  const botonDarkMode = document.querySelector(".dark-mode-boton");
+  botonDarkMode.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
 }
 
 function eventListeners() {
-    const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-    mobileMenu.addEventListener('click', navegacionResponsive);
+  mobileMenu.addEventListener("click", navegacionResponsive);
 }
 
 function navegacionResponsive() {
-    const navegacion = document.querySelector('.navegacion');
+  const navegacion = document.querySelector(".navegacion");
 
-    navegacion.classList.toggle('mostrar')
+  navegacion.classList.toggle("mostrar");
 }
+// Create the back button element
+const backButtonContainer = document.getElementById("back-button-container");
+const backButton = document.createElement("button");
+backButton.setAttribute("id", "back-button");
+backButton.textContent = "Back to Portfolio";
+
+// Add an event listener to the back button
+backButton.addEventListener("click", () => {
+  // Change the URL to your portfolio URL
+  window.location.href = "https://your-portfolio-website.com";
+});
+
+// Append the back button to the container
+backButtonContainer.appendChild(backButton);
